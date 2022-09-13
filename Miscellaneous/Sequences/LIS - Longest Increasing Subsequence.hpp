41bc9a1c03141baf2b45309099b0f2535174ac44
alysonNBS <alyson_nbs2.0@hotmail.com>
2022-09-13 10:42:39 -0300

    number of LIS

--- Miscellaneous/Sequences/LIS - Longest Increasing Subsequence.hpp
+++ Miscellaneous/Sequences/LIS - Longest Increasing Subsequence.hpp
@@ -13,3 +13,31 @@ int lengthOfLIS(vector<int>& nums) {
     }
     return length;
 }
+
+// time-complexity: O(n^2)
+// space-complexity: O(n)
+int findNumberOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> dp(2, vector<int>(n, 1));
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < i; ++j)
+            if (nums[i] > nums[j])
+                dp[0][i] = max(dp[0][i], dp[0][j] + 1);
+    
+    int lengthOfLIS = *max_element(dp[0].begin(), dp[0].end());
+    int answer {0};
+    
+    for (int i = 0; i < n; ++i)
+        if (dp[0][i] != 1)
+            dp[1][i] = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (dp[0][i] == dp[0][j] + 1 && nums[i] > nums[j])
+                dp[1][i] += dp[1][j];
+        }
+        if (dp[0][i] == lengthOfLIS)
+            answer += dp[1][i];
+    }
+    
+    return answer;
+}
