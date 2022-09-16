6391d5307434c76ead77104e0fdcc6327792a271
alysonNBS <alyson_nbs2.0@hotmail.com>
2022-09-16 13:54:12 -0300

    update number of LIS from O(n^2) to O(n log n) using Segtree

--- Miscellaneous/Sequences/LIS - Longest Increasing Subsequence.hpp
+++ Miscellaneous/Sequences/LIS - Longest Increasing Subsequence.hpp
@@ -14,30 +14,80 @@ int lengthOfLIS(vector<int>& nums) {
     return length;
 }
 
-// time-complexity: O(n^2)
+// time-complexity: O(n log n)
 // space-complexity: O(n)
+class SegtreeToLIS {
+    int size; // size is the 1st 2-pow greater or equal n
+    vector<pair<int, int>> tree;
+
+public:
+    SegtreeToLIS(int n) {
+        size = 1;
+        while (size < n)
+            size <<= 1;
+        tree.assign(size << 1, {0, 0});
+        this->update(0, {0, 1});
+    }
+
+    void update(int index, pair<int, int> value) {
+        return update(index, value, 0, 0, size);
+    }
+
+    void update(int index, pair<int, int> value, int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            if (value.first != tree[x].first) tree[x] = value;
+            else tree[x] = {tree[x].first, tree[x].second + value.second};
+            return;
+        }
+        int mid = (lx + rx) >> 1;
+        if (index < mid) update(index, value, (x << 1) + 1, lx, mid);
+        else update(index, value, (x << 1) + 2, mid, rx);
+        
+        if (tree[(x << 1) + 1].first > tree[(x << 1) + 2].first) tree[x] = tree[(x << 1) + 1];
+        else if (tree[(x << 1) + 1].first < tree[(x << 1) + 2].first) tree[x] = tree[(x << 1) + 2];
+        else tree[x] = {tree[(x << 1) + 1].first, tree[(x << 1) + 1].second + tree[(x << 1) + 2].second};
+    }
+
+    pair<int, int> find(int index) {
+        return find(index, 0, 0, size);
+    }
+
+    pair<int, int> find(int index, int x, int lx, int rx) {
+        if (lx >= index) return {0, 0};
+        if (rx <= index) return tree[x];
+        int mid = (lx + rx) >> 1;
+        auto left = find(index, (x << 1) + 1, lx, mid);
+        auto right = find(index, (x << 1) + 2, mid, rx);
+
+        if (left.first > right.first) return left;
+        else if (right.first > left.first) return right;
+        else return {left.first, left.second + right.second};
+    }
+};
+
 int findNumberOfLIS(vector<int>& nums) {
     int n = nums.size();
-    vector<vector<int>> dp(2, vector<int>(n, 1));
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < i; ++j)
-            if (nums[i] > nums[j])
-                dp[0][i] = max(dp[0][i], dp[0][j] + 1);
-    
-    int lengthOfLIS = *max_element(dp[0].begin(), dp[0].end());
-    int answer {0};
-    
-    for (int i = 0; i < n; ++i)
-        if (dp[0][i] != 1)
-            dp[1][i] = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < i; ++j) {
-            if (dp[0][i] == dp[0][j] + 1 && nums[i] > nums[j])
-                dp[1][i] += dp[1][j];
+    SegtreeToLIS segtree(n + 1);
+    //compress numbers from 1 to n
+    {
+        map<int,list<int>> mp;
+        for (int i = 0; i < n; ++i) {
+            if (mp.find(nums[i]) == mp.end())
+                mp[nums[i]] = list<int>();
+            mp[nums[i]].push_back(i);
         }
-        if (dp[0][i] == lengthOfLIS)
-            answer += dp[1][i];
+        int compress_num = 1;
+        for (const auto& pair: mp) {
+            for (const auto& index : pair.second)
+                nums[index] = compress_num;
+            ++compress_num;
+        }
     }
     
-    return answer;
+    for (const auto& num : nums) {
+        auto element = segtree.find(num);
+        segtree.update(num, {element.first + 1, element.second});
+    }
+
+    return segtree.find(n + 1).second;
 }
